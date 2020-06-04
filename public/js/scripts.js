eed01f01d1617d76bce56e525e9c84e38c046508
Rafael Herrera <santoElias65537@gmail.com>
2020-06-04 00:06:48 -0600

    added slide show to the main pic

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,67 +3,91 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
     */
-    (function($) {
-    "use strict"; // Start of use strict
-  
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: (target.offset().top - 71)
-          }, 1000, "easeInOutExpo");
-          return false;
-        }
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 71)
+        }, 1000, "easeInOutExpo");
+        return false;
       }
+    }
+  });
+
+  // Scroll to top button appear
+  $(document).scroll(function () {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 80
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Floating label headings for the contact form
+  $(function () {
+    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function () {
+      $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function () {
+      $(this).removeClass("floating-label-form-group-with-focus");
     });
-  
-    // Scroll to top button appear
-    $(document).scroll(function() {
-      var scrollDistance = $(this).scrollTop();
-      if (scrollDistance > 100) {
-        $('.scroll-to-top').fadeIn();
-      } else {
-        $('.scroll-to-top').fadeOut();
-      }
-    });
-  
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
-      $('.navbar-collapse').collapse('hide');
-    });
-  
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-      target: '#mainNav',
-      offset: 80
-    });
-  
-    // Collapse Navbar
-    var navbarCollapse = function() {
-      if ($("#mainNav").offset().top > 100) {
-        $("#mainNav").addClass("navbar-shrink");
-      } else {
-        $("#mainNav").removeClass("navbar-shrink");
-      }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-  
-    // Floating label headings for the contact form
-    $(function() {
-      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-      }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-      }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-      });
-    });
-  
-  })(jQuery); // End of use strict
-  
+  });
+
+})(jQuery); // End of use strict
+
+
+const relativeImgPath = ["assets\\img\\portfolio\\cabin.png", "assets\\img\\portfolio\\cake.png", "assets\\img\\portfolio\\circus.png", "assets\\img\\portfolio\\game.png", "assets\\img\\portfolio\\safe.png", "assets\\img\\portfolio\\submarine.png", "assets\\img\\avataaars.svg"]
+
+const mastHeadMain = document.getElementById("mastHeadMain");
+
+for (let i = 0; i < relativeImgPath.length; i++) {
+  setTimeout(() => {
+    console.log(i);
+    mastHeadMain.src = relativeImgPath[i]
+
+  }, i * 3000);
+
+  //mastHeadMain.src = relativeImgPath[i];
+}
+//
+// todo
+//
+// for back  transiotio nunderstand code below and customaize
+// .slide {
+//   -webkit-transition: opacity 2000ms ease-in-out;
+//      -moz-transition: opacity 2000ms ease-in-out;
+//        -o-transition: opacity 2000ms ease-in-out;
+//           transition: opacity 2000ms ease-in-out;
+//  }
--- src/main.js
+++ src/main.js
@@ -6,3 +6,31 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+console.log("here")
+
+// what to do
+//console.log all images in public.assets.portfiolio
+
+//get  Masthead Avatar Image
+
+//for loop through all publick/assets/portfoliol.length
+
+//settimeout every two seconds and change src of masthead aceater image
+
+//found on stack overflow, this should work
+// const { readdirSync, statSync } = require('fs')
+// const { join } = require('path')
+
+// const dirs = p => readdirSync(p).filter(f => statSync(join(p, f)).isDirectory())
+
+
+// or
+// have Array
+
+// const relativeImgPath = []
+
+// for(let i = 0; i < relativeImgPath.length; i++) {
+  // setTimeout for every two seconds
+//   masterHeadSrc = `${relativeImgPath[i]}`
+// }
